f0ae3059f2a4b7d607d0e956abf046ea80e7bd8d
jeongyun <jeong.yun@tukorea.ac.kr>
2024-03-11 11:00:59 +0900

    style : add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'priamry': '#1C0F38',
         'timer-bg': '#FDDCDC', 
         'timer-stop': '#FF8484',
-        'timer-start': '#58AFF9'
+        'timer-start': '#58AFF9',
+        'timer-number': '#E4C7C7'
       }
     },
   },
