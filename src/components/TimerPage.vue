790c2330de447f315da9cf6d4666e503e1481ae5
jeongyun <jeong.yun@tukorea.ac.kr>
2024-03-11 15:35:12 +0900

    style : change css style in timer page

--- src/components/TimerPage.vue
+++ src/components/TimerPage.vue
@@ -10,18 +10,18 @@
       </div>
     </div>
     <div class="w-full h-full flex items-start justify-center">
-      <div class="bg-white min-h-60 min-w-96 max-w-6xl py-7 h-auto w-7/12 rounded-2xl flex flex-col justify-center items-center gap-6">
-        <div class="text-5xl sm:text-4xl md:text-6xl lg:text-7xl text-timer-stop font-['DS-Digital']">06.00</div>
-        <div class="w-8/12 h-40 bg-timer-bg rounded-2xl border-priamry border-[5px] flex items-center justify-center">
-          <div class="text-8xl sm:text-7xl md:text-9xl text-timer-number font-['DS-Digital']">88.88</div>
-          <div class="text-8xl sm:text-7xl md:text-9xl text-priamry -ml-[212.8px] sm:-ml-[159.5px] md:-ml-[283.5px] font-['DS-Digital']">02.37</div>
+      <div class="bg-white min-h-60 min-w-96 max-w-6xl py-5 md:py-6 lg:py-7 h-auto w-7/12 rounded-2xl flex flex-col justify-center items-center gap-3 sm:gap-4 md:gap-5">
+        <div class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-timer-stop font-['DS-Digital']">06.00</div>
+        <div class="w-8/12 h-auto py-4 sm:py-5 md:py-6 lg:py-7 bg-timer-bg rounded-2xl border-priamry border-4 md:border-[5px] flex items-center justify-center">
+          <div class="text-7xl sm:text-8xl md:text-9xl text-timer-number font-['DS-Digital']">88.88</div>
+          <div class="text-7xl sm:text-8xl md:text-9xl text-priamry -ml-[159.5px] sm:-ml-[212.8px] md:-ml-[283.5px] font-['DS-Digital']">02.37</div>
         </div>
         <div class="flex justify-center gap-4">
-          <div class="w-24 h-12 hover:bg-timer-start hover:text-white text-timer-start border-timer-start border-2 flex items-center justify-center rounded-md mx-4">
-            <div class="text-2xl font-semibold hover:cursor-pointer">START</div>
+          <div class="w-20 md:w-24 h-10 md:h-12 hover:bg-timer-start hover:text-white text-timer-start border-timer-start border-2 flex items-center justify-center rounded-md mx-4">
+            <div class="text-l md:text-2xl font-semibold hover:cursor-pointer">START</div>
           </div>
-          <div class="w-24 h-12 bg-timer-stop text-white flex items-center justify-center rounded-md mx-4">
-            <div class="text-2xl font-semibold hover:cursor-pointer">STOP</div>
+          <div class="w-20 md:w-24 h-10 md:h-12 bg-timer-stop text-white flex items-center justify-center rounded-md mx-4">
+            <div class="text-l md:text-2xl font-semibold hover:cursor-pointer">STOP</div>
           </div>
         </div>
       </div>
