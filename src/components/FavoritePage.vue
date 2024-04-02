72c570529e4096a689feb2c8095aeefef02c7597
min-minnn <alswlk0306@gmail.com>
2024-04-03 03:45:37 +0900

    style: vs icon style

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-col h-screen w-full bg-priamry">
+  <div class="flex flex-col h-screen w-full bg-primary">
     <div class="w-full h-24 flex items-center">
       <div class="text-white text-xl pl-6 font-semibold hover:cursor-pointer">PLAY - TINO</div>
     </div>
-    <div class="w-full h">
+    <div class="w-full">
       <div class="w-full h-24 flex justify-center items-center">
         <div class="flex items-baseline">
           <div class="text-4xl md:text-5xl lg:text-7xl text-favorite-title font-extrabold text-outline">고냥이</div>
@@ -12,7 +12,7 @@
       </div>
       <div class="w-full text-white text-xl flex justify-center items-center">16강</div>
     </div>
-    <div class="w-full h-full min-w-1 mt-2 flex justify-center">
+    <div class="w-full h-full mt-2 flex justify-center relative">
       <div class="w-1/3 flex flex-col justify-center items-end cursor-pointer">
         <img class="border-8" src="/image/cat1.png" />
         <div class="flex items-center justify-center w-full text-white text-2xl mt-3">고급묘다냥 샴 고양이</div>
@@ -21,7 +21,9 @@
         <img class="border-8" src="/image/cat2.png" />
         <div class="flex items-center justify-center w-full text-white text-2xl mt-3">왕 크니까 왕 귀엽다 메인쿤</div>
       </div>
-      <img class="fixed right-47% top-31.5rem w-24 h-24" src="/image/vs.png" />
+      <div class="absolute inset-0 flex items-center justify-center">
+        <img class="w-24 h-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="/image/vs.png" />
+      </div>
     </div>
   </div>
 </template>
