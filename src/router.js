c23728734b404f4a900c0f11e56e8642c17c04ab
min-minnn <alswlk0306@gmail.com>
2024-04-08 18:11:47 +0900

    feat: add favorite statistic page router

--- src/router.js
+++ src/router.js
@@ -11,6 +11,8 @@ const routes = [
   component: () => import("./components/FavoritePage.vue")},
   { path: "/comment",
   component: () => import("./components/CommentPage.vue")},
+  { path: "/favorite-statistic",
+  component: () => import('./components/FavoriteStatisticPage.vue')},
   { path: "/timer-statistic",
   component: () => import("./components/TimerStatisticPage.vue")},
   { path: "/quiz-statistic",
