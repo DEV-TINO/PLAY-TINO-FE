2606bd193d6dc69883319b01809b147101e3cde0
min-minnn <alswlk0306@gmail.com>
2024-04-10 01:29:47 +0900

    feat: make favorite game finish modal page

---
+++ src/components/FavoriteFinishModal.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="w-full h-full bg-modal-background bg-opacity-50 fixed flex justify-center items-center z-50">
+		<div class="bg-white rounded-3xl">
+			<div class="flex flex-col items-center pt-10 pb-10 pr-32 pl-32 m-4 border-2 border-favorite-theme rounded-3xl">
+				<div class="text-5xl font-extrabold text-favorite-theme mb-5">고냥이 월드컵!</div>
+				<div class="text-2xl font-bold text-favorite-theme mb-2">당신의 1등 고양이는</div>
+				<div class="flex flex-col items-center mb-5">
+					<img class="w-56 border-4 border-favorite-theme mb-2" src="/image/cat1.png" />
+					<div class="text-xl font-semibold text-primary">고급묘다냥 샴 고양이</div>
+				</div>
+				<div class="flex items-center justify-between">
+					<div class="bg-favorite-theme text-white border-favorite-theme border-2 pr-4 pl-4 pt-1 pb-1 rounded-lg flex justify-center font-bold text-lg">확인</div>
+					<div class="bg-white border-favorite-theme border-2 pr-4 pl-4 pt-1 pb-1 mr-2 ml-2 rounded-lg text-favorite-theme font-bold text-xl">다시하기</div>
+					<div class="bg-white border-favorite-theme border-2 pr-4 pl-4 pt-1 pb-1 rounded-lg text-favorite-theme font-bold text-xl">메인으로</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
