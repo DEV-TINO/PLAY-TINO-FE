50df108462f5f1fe60328e22177f1f797a8b3d47
min-minnn <alswlk0306@gmail.com>
2024-04-10 22:49:04 +0900

    feat: load rank data use api

--- src/components/FavoriteStatisticPage.vue
+++ src/components/FavoriteStatisticPage.vue
@@ -14,20 +14,17 @@
       <div v-for="rank, i in 3" :key="i"  class="flex justify-center pb-8">
         <div class="w-3/4 flex justify-around items-center">
           <div class="w-16 h-16 flex justify-center items-center p-4 text-light-purple text-4xl font-bold">{{ i + 1 }}</div>
-          <img class="w-48 border-8 border-light-purple" src="/image/cat1.png" />
-          <div class="text-primary text-3xl">고급묘다냥 샴 고양이</div>
-          <div class="w-1/3 bg-gray-200 rounded-lg dark:bg-gray-700">
-            <div class="pt-3 pb-3 bg-light-purple text-xl font-medium text-primary text-center p-0.5 leading-none rounded-lg" style="width: 45%">45%</div>
+          <img class="w-48 h-48 border-8 border-light-purple" :src="this.$store.state.favoriteRankData[i]?.favoriteImage" />
+          <div class="w-80 flex justify-center text-primary text-3xl">{{ this.$store.state.favoriteRankData[i]?.favoriteTitle }}</div>
+          <div class="w-1/3 bg-gray-200 rounded-xl dark:bg-gray-700">
+            <div class="pt-3 pb-3 bg-light-purple text-xl font-medium text-primary text-center p-0.5 leading-none rounded-xl" :style="`width: ${ this.$store.state.favoriteRankData[i]?.favoriteRankPercentage }`">{{ this.$store.state.favoriteRankData[i]?.favoriteRankPercentage }}%</div>
           </div>
         </div>
       </div>
       <div class="flex justify-center w-full">
         <div class="flex items-center justify-between w-1/6 text-2xl">
           <font-awesome-icon class="text-gray-200 cursor-pointer active:text-gray-200 hover:text-primary" :icon="['fas', 'angle-left']" />
-          <div class="font-bold text-primary cursor-pointer active:text-primary">1</div>
-          <div class="font-bold text-gray-200 cursor-pointer active:text-primary">2</div>
-          <div class="font-bold text-gray-200 cursor-pointer active:text-primary">3</div>
-          <div class="font-bold text-gray-200 cursor-pointer active:text-primary">4</div>
+          <div v-for="i in 4" :key="i" @click="this.$store.dispatch('getFavoriteRank', i)" :class="this.$store.state.rankPage == i ? 'selected-rank-page' : 'unselected-rank-page'" class="font-bold cursor-pointer active:text-primary hover:text-primary">{{ i }}</div>
           <font-awesome-icon class="text-gray-200 cursor-pointer active:text-gray-200 hover:text-primary" :icon="['fas', 'angle-right']" />    
         </div>
       </div>
@@ -43,6 +40,9 @@ export default {
   components: {
     CommentPage,
   },
+  mounted() {
+    this.$store.dispatch('getFavoriteRank', 1)
+  },
   methods: {
     handleRouterMain() {
 			this.$router.push(`/`)
@@ -52,4 +52,10 @@ export default {
 </script>
 
 <style>
+.selected-rank-page {
+  color: '#1C0F38'
+}
+.unselected-rank-page {
+  color: lightgray
+}
 </style>
