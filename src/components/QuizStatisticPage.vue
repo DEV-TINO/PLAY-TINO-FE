fc3fdb00abbc9c2ff7ef6aef2f21790711cb46d9
jeongyun <jeong.yun@tukorea.ac.kr>
2024-04-11 21:58:05 +0900

    feat : get rank data from api and display

--- src/components/QuizStatisticPage.vue
+++ src/components/QuizStatisticPage.vue
@@ -10,15 +10,15 @@
         <div class="text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold w-4/12 flex justify-center items-center min-w-48">맞춘 문제</div>
         <div class="text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold w-2/12 flex justify-center items-center min-w-16">총점</div>
       </div>
-      <div>
+      <div v-for="(user, index) in currentPageData" :key="user.quizRankId">
         <div class="flex pb-4 md:pb-7">
-          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">1</div>
+          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">{{ index + 1 }}</div>
           <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
           <div class="w-4/12 min-w-48 flex gap-2 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
             <div class="flex gap-2">
               <div>넌센스</div>
               <div class="flex">
-                <div>5</div>
+                <div>{{ user.nonsenseCorrect }}</div>
                 <div>개</div>
               </div>
             </div>
@@ -26,125 +26,13 @@
             <div class="flex gap-2">
               <div>상식</div>
               <div class="flex">
-                <div>5</div>
+                <div>{{ user.commonsenseCorrect }}</div>
                 <div>개</div>
               </div>
             </div>
           </div>
           <div class="w-2/12 flex justify-center items-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl min-w-16 gap-1">
-            <div>10</div>
-            <div>/</div>
-            <div>10</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="flex pb-4 md:pb-7">
-          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">2</div>
-          <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
-          <div class="w-4/12 min-w-48 flex gap-2 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
-            <div class="flex gap-2">
-          <div>넌센스</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-            <div>/</div>
-            <div class="flex gap-2">
-              <div>상식</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 flex justify-center items-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl min-w-16 gap-1">
-            <div>10</div>
-            <div>/</div>
-            <div>10</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="flex pb-4 md:pb-7">
-          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">3</div>
-          <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
-          <div class="w-4/12 min-w-48 flex gap-2 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
-            <div class="flex gap-2">
-              <div>넌센스</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-            <div>/</div>
-            <div class="flex gap-2">
-              <div>상식</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 flex justify-center items-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl min-w-16 gap-1">
-            <div>10</div>
-            <div>/</div>
-            <div>10</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="flex pb-4 md:pb-7">
-          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">4</div>
-          <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
-          <div class="w-4/12 min-w-48 flex gap-2 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
-            <div class="flex gap-2">
-              <div>넌센스</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-            <div>/</div>
-            <div class="flex gap-2">
-              <div>상식</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 flex justify-center items-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl min-w-16 gap-1">
-            <div>10</div>
-            <div>/</div>
-            <div>10</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="flex pb-4 md:pb-7">
-          <div class="w-2/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">5</div>
-          <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
-          <div class="w-4/12 min-w-48 flex gap-2 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
-            <div class="flex gap-2">
-              <div>넌센스</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-            <div>/</div>
-            <div class="flex gap-2">
-              <div>상식</div>
-              <div class="flex">
-                <div>5</div>
-                <div>개</div>
-              </div>
-            </div>
-          </div>
-          <div class="w-2/12 flex justify-center items-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl min-w-16 gap-1">
-            <div>10</div>
+            <div>{{ user.allCorrect }}</div>
             <div>/</div>
             <div>10</div>
           </div>
@@ -152,10 +40,9 @@
       </div>
       <div class="flex items-center justify-center gap-2 md:gap-3 lg:gap-4 text-sm md:text-lg lg:text-xl">
         <font-awesome-icon class="text-primary text-base" :icon="['fas', 'angle-left']" />
-        <div class="font-bold text-primary"><u>1</u></div>
-        <div class="font-bold text-gray-200">2</div>
-        <div class="font-bold text-gray-200">3</div>
-        <div class="font-bold text-gray-200">4</div>
+        <div v-for="(page, pageNumber) in pageCount" :key="pageNumber" :class="{ 'font-bold text-primary': currentPage == pageNumber + 1, 'font-bold text-gray-200': currentPage != pageNumber + 1 }" @click="changePage(pageNumber + 1)">
+          <u>{{ pageNumber + 1 }}</u>
+        </div>
         <font-awesome-icon class="text-primary text-base":icon="['fas', 'angle-right']" />
       </div>
     </div>
@@ -165,9 +52,40 @@
     
   <script>
     import CommentPage from '../components/CommentPage.vue'
+    import axios from 'axios'
     export default {
       components:{
           CommentPage,
+      },
+      data() { 
+        return {
+          rankData: [],
+          currentPage: 1,
+          itemsPerPage: 5,
+        }
+      },
+      computed: {
+        pageCount() {
+          return Math.ceil(this.rankData.length / this.itemsPerPage)
+        },
+        currentPageData() {
+          const startIndex = (this.currentPage - 1) * this.itemsPerPage
+          const endIndex = startIndex + this.itemsPerPage
+          return this.rankData.slice(startIndex, endIndex)
+        }
+      },
+      methods: {
+        async getRankData() {
+          const response = await axios.get(`${this.$store.state.quizPort}/quiz/rank/all?page=`)
+          const obj = response.data
+          this.rankData = obj
+        },
+        changePage(pageNumber) {
+          this.currentPage = pageNumber
+        },
+      },
+      mounted() {
+        this.getRankData()
       },
     }
   </script>
