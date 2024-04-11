4bff31aa40f1e8e50d61ad3e360fc5bff56a85bc
jeongyun <jeong.yun@tukorea.ac.kr>
2024-04-11 21:56:02 +0900

    feat : get quiz by api and display

--- src/components/QuizPage.vue
+++ src/components/QuizPage.vue
@@ -17,28 +17,29 @@
     <div class="w-full h-full flex items-start justify-center bg-primary">
       <div class="bg-white min-h-60 min-w-96 max-w-6xl py-6 md:py-8 lg:py-10 xl:py-12 h-full w-7/12 rounded-2xl flex flex-col justify-center items-center gap-2 md:gap-3">
         <div class="flex w-11/12">
-          <div class="bg-quiz-theme text-white text-base sm:text-lg md:text-xl xl:text-2xl w-20 sm:w-24 md:w-28 xl:w-32 h-8 sm:h-9 md:h-10 xl:h-12 flex items-center justify-center rounded-xl">넌센스</div>
+          <div class="bg-quiz-theme text-white text-base sm:text-lg md:text-xl xl:text-2xl w-20 sm:w-24 md:w-28 xl:w-32 h-8 sm:h-9 md:h-10 xl:h-12 flex items-center justify-center rounded-xl">{{ this.currentQuiz.category }}</div>
         </div>
-        <div class="flex border-quiz-box border-[3px] py-6 md:py-8 xl:py-10 px-6 rounded-xl w-11/12">
-          <div class="min-w-14 xl:min-w-16 text-lg md:text-xl xl:text-2xl font-semibold text-primary">Q 12.</div>
-          <div class="text-lg md:text-xl xl:text-2xl font-semibold text-primary">동생과 형이 싸우는데 엄마가 동생 편만 드는 세상은?</div>
+        <div class="flex border-quiz-box border-3 py-6 md:py-8 xl:py-10 px-6 rounded-xl w-11/12">
+          <div class="min-w-14 xl:min-w-16 text-lg md:text-xl xl:text-2xl font-semibold text-primary">Q {{ this.quizCount }}.</div>
+          <div class="text-lg md:text-xl xl:text-2xl font-semibold text-primary">{{ this.currentQuiz.question }}</div>
         </div>
         <div class="flex gap-2 text-gray-500 w-11/12 pt-4 md:pt-6">
           <div class="text-sm md:text-base xl:text-lg">Hint:</div>
-          <div class="text-sm md:text-base xl:text-lg">평소에 형에 대해 어떻게 생각하셨나요?</div>
+          <div class="text-sm md:text-base xl:text-lg">{{ this.currentQuiz.hint }}</div>
         </div>
         <div class="flex w-11/12 gap-4">
-          <textarea placeholder="정답을 작성해주세요" class="border-primary overflow-hidden rounded-xl border-2 w-full h-12 pt-2 px-3 text-lg md:text-xl resize-none font-semibold"></textarea>
-          <div @click="handleClickTitle($event)" class="bg-quiz-theme text-white font-bold w-14 rounded-lg flex justify-center items-center text-2xl">
+          <input @keyup.enter="handleOpenModal($event)" v-model="answer" placeholder="정답을 작성해주세요" type="text" class="border-primary overflow-hidden rounded-xl border-2 w-full h-12 py-2 px-3 text-lg md:text-xl resize-none font-semibold"></input>
+          <div @click="handleOpenModal($event)" class="bg-quiz-theme text-white font-bold w-14 rounded-lg flex justify-center items-center text-2xl">
             <font-awesome-icon :icon="['fas', 'angle-right']" />
           </div>
         </div>
       </div>
     </div>
     <div class="bg-primary w-full h-full flex items-start justify-center py-5">
-      <div class="flex gap-1 items-baseline justify-center w-24 h-8 md:w-32 md:h-9 bg-quiz-theme rounded-xl">
-        <div class="text-white font-medium text-xl md:text-2xl">8</div>
-        <div class="text-white text-sm md:text-lg">/10</div>
+      <div class="flex gap-1 items-center justify-center w-24 h-8 md:w-32 md:h-9 bg-quiz-theme rounded-xl">
+        <div class="text-white font-medium text-xl md:text-2xl">{{ this.quizCount }}</div>
+        <div class="text-white text-xl"> / </div>
+        <div class="text-white text-sm md:text-lg">{{ this.quizNumber }}</div>
       </div>
     </div>
     <div class="bg-progress-bar-light w-full h-full min-h-1 max-h-2">
@@ -52,18 +53,47 @@ import ModalCard from '../components/QuizWrongModal.vue'
   export default {
     components:{
       ModalCard,
+import axios from 'axios'
   },
     data() { 
       return {
         openModal: false,
         progressbarHandler: null,
         timeBegan: null,
+        quizNumber: 10,
+        quizCount: 1,
+        quizzes: [],
+        currentQuiz: [],
+        answer: '', 
+        nonsenseCorrect: 0,
+        commonCorrect: 0,
         MAX_TIME: 15000, // 15s
       }
   },
   methods: {
-    handleClickTitle(index){
-      this.openModal = true
+    async getQuiz() {
+        const response = await axios.get(`${this.$store.state.quizPort}/quiz/start/user/3978099b-419d-46cb-a2ca-258b7f7ee535`)
+        const obj = response.data.responseQuizList
+        this.quizzes = JSON.parse(obj)
+        this.currentQuiz = this.quizzes[this.quizCount - 1]
+    },
+    loadNextQuiz() {
+      if(this.quizCount < this.quizNumber) {
+        this.quizCount++
+        this.currentQuiz = this.quizzes[this.quizCount - 1]
+        this.answer=''
+      }
+    },
+    isCorrect() {
+      const strippedAnswer = this.answer.trim().replace(/\n|\r|\s*/g, "")
+      const strippedCorrectAnswer = String(this.currentQuiz.answer).trim().replace(/\n|\r|\s*/g, "")
+      if (strippedAnswer == strippedCorrectAnswer) {
+        this.modalComponent = 'CorrectModalCard'
+        if (this.currentQuiz.category == "상식") this.commonCorrect++
+        else this.nonsenseCorrect++
+      } else {
+        this.modalComponent = 'WrongModalCard'
+      }
     },
     progressBarStart() {
       this.timeBegan = new Date()
@@ -101,6 +131,7 @@ import ModalCard from '../components/QuizWrongModal.vue'
   },
   mounted() {
     this.progressBarStart()
+    this.getQuiz()
   },
 }
 </script>
