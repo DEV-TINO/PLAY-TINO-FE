92a98b4c54ba723a8b55715506f85ab03a4570f0
jeongyun <jeong.yun@tukorea.ac.kr>
2024-04-13 12:31:41 +0900

    feat : make page changing works

--- src/components/TimerStatisticPage.vue
+++ src/components/TimerStatisticPage.vue
@@ -17,9 +17,9 @@
           <div class="text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold w-3/12 flex justify-center items-center min-w-40">기록 / 목표 시간</div>
           <div class="text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold w-1/12 flex justify-center items-center min-w-14">오차</div>
         </div>
-        <div v-for="(user, index) in currentPageData" key="user.quizRankId">
+        <div v-for="(user, index) in this.rankData" key="user.quizRankId">
           <div class="flex pb-4 md:pb-7">
-            <div class="w-3/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">{{ startIndex + index + 1 }}</div>
+            <div class="w-3/12 min-w-10 flex justify-end px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20 text-light-purple text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold items-center">{{ this.getRank(index) }}</div>
             <div class="w-3/12 min-w-40 items-center pl-1 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">alswlfjddl</div>
             <div class="w-3/12 min-w-40 flex gap-3 justify-center text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl items-center">
               <div>{{ user.stopTime }}</div>
@@ -54,37 +54,41 @@
         rankData: [],
         currentPage: 1,
         itemsPerPage: 5,
-        startIndex: 0,
+        totalRank: 0,
       }
     },
     computed: {
       pageCount() {
-        return Math.ceil(this.rankData.length / this.itemsPerPage)
+        return Math.ceil(this.totalRank / this.itemsPerPage)
       },
-      currentPageData() {
-        this.startIndex = (this.currentPage - 1) * this.itemsPerPage
-        const endIndex = this.startIndex + this.itemsPerPage
-        return this.rankData.slice(this.startIndex, endIndex)
-      }
     },
-    methods:{
-      async getRankData() {
-        const response = await axios.get(`${this.$store.state.timerPort}/timer/rank/all?page=`)
-        this.rankData = response.data.content
+    methods: {
       handleRouterMain() {
         this.$router.push(`/`)
       },
+      async getRankData(pageNumber) {
+        const response = await axios.get(`${this.$store.state.timerPort}/timer/rank/all?page=${pageNumber ?? 0}`)
+        this.rankData = response.data.timerList
+        this.totalRank = response.data.timerTotal - 1
+      },
+      getRank(index){
+        return (this.currentPage - 1) * 5 + index + 1
       },
       changePage(pageNumber) {
         this.currentPage = pageNumber
+        this.getRankData(this.currentPage - 1)
       },
       decressePageNumber() {
-        if (this.currentPage > 1)
-        this.currentPage -= 1
+        if (this.currentPage > 1) {
+          this.currentPage -= 1
+          this.getRankData(this.currentPage - 1)
+        }
       },
       increasePageNumber() {
-        if (this.currentPage < this.pageCount)
-        this.currentPage += 1
+        if (this.currentPage < this.pageCount) {
+          this.currentPage += 1
+          this.getRankData(this.currentPage - 1)
+        }
       }
     },
     mounted() {
