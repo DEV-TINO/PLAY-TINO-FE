55c72b8a91a9565e926d71307ed87ef063226f7a
min-minnn <alswlk0306@gmail.com>
2024-04-13 11:12:21 +0900

    feat: favorite game animation and make game

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -1,35 +1,37 @@
 <template>
-  <Transition>
-    <FinishModal v-if="this.$store.state.favoriteRoundFinish"></FinishModal>
-  </Transition>
-  <div class="flex flex-col h-screen w-full bg-primary">
-    <div class="w-full h-24 flex items-center">
-      <div class="text-white text-xl pl-6 font-semibold hover:cursor-pointer min-w-40">PLAY - TINO</div>
-    </div>
-    <div class="w-full sm:min-h-32 flex flex-col justify-end">
-      <div class="w-full h-24 flex justify-center items-center">
-        <div class="flex items-baseline min-w-52">
-          <div class="text-4xl md:text-5xl lg:text-7xl text-favorite-theme font-extrabold text-outline">고냥이</div>
-          <div class="text-xl md:text-2xl lg:text-4xl text-white font-bold">월드컵!</div>
-        </div>
+  <div>
+    <Transition>
+      <FinishModal v-if="$store.state.favoriteRoundFinish"></FinishModal>
+    </Transition>
+    <div class="flex flex-col h-screen w-full bg-primary">
+      <div class="w-full h-24 flex items-center">
+        <div class="text-white text-xl pl-6 font-semibold hover:cursor-pointer min-w-40">PLAY - TINO</div>
       </div>
-      <div class="w-full text-white text-xl flex justify-center items-center">{{ this.$store.state.favoriteGameRound }}강</div>
-    </div>
-    <div class="w-full h-full mt-2 flex justify-center relative min-w-min">
-      <div @click="this.$store.commit('selectFavoriteImg', 1)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 1, 'unselected-left': this.$store.state.favoriteSelectedImg === 2 }" class="flex flex-col justify-center items-end cursor-pointer">
-        <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 1 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-          <img class="object-cover w-full h-full" :src="this.$store.state.favoriteList[0]?.favoriteImage" />
+      <div class="w-full sm:min-h-32 flex flex-col justify-end">
+        <div class="w-full h-24 flex justify-center items-center">
+          <div class="flex items-baseline min-w-52">
+            <div class="text-4xl md:text-5xl lg:text-7xl text-favorite-theme font-extrabold text-outline">고냥이</div>
+            <div class="text-xl md:text-2xl lg:text-4xl text-white font-bold">월드컵!</div>
+          </div>
         </div>
-        <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === 1, 'hidden': this.$store.state.favoriteSelectedImg === 2 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl">{{ this.$store.state.favoriteList[0]?.favoriteTitle }}</div>
+        <div class="w-full text-white text-xl flex justify-center items-center">{{ $store.state.favoriteGameRound }}</div>
       </div>
-      <div @click="this.$store.commit('selectFavoriteImg', 2)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 2, 'unselected-right': this.$store.state.favoriteSelectedImg === 1 }" class="flex flex-col justify-center cursor-pointer">
-        <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 2 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-          <img class="object-cover w-full h-full" :src="this.$store.state.favoriteList[1]?.favoriteImage" />
+      <div class="w-full h-full mt-2 flex justify-center relative min-w-min">
+        <div @click="selectedImg(0)" :class="{ 'selected-left': $store.state.favoriteSelectedImg === 0 }" class="flex flex-col justify-center items-end cursor-pointer">
+          <div :class="{ 'selected-left': $store.state.favoriteSelectedImg === 0, 'unselected-left': $store.state.favoriteSelectedImg === 1 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
+            <img class="object-cover w-full h-full" :src="currentPair.image1" />
+          </div>
+          <div :class="{ 'selected-left-text': $store.state.favoriteSelectedImg === 0, 'hidden':  $store.state.favoriteSelectedImg === 1 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl">{{ currentPair.title1 }}</div>
         </div>
-        <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === 2, 'hidden': this.$store.state.favoriteSelectedImg === 1 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl">{{ this.$store.state.favoriteList[1]?.favoriteTitle }}</div>
-      </div>
-      <div v-if="this.$store.state.favoriteSelectedImg == 0" class="h-24 absolute top-72 inset-0 flex items-center justify-center">
-        <img v-show="this.$store.state.favoriteSelectedImg == 0" class="w-24 h-24 absolute sm:-translate-y-18" src="/image/vs.png" />
+        <div @click="selectedImg(1)" :class="{ 'selected-right': $store.state.favoriteSelectedImg === 1 }" class="flex flex-col justify-center cursor-pointer">
+          <div :class="{ 'selected-right': $store.state.favoriteSelectedImg === 1, 'unselected-right': $store.state.favoriteSelectedImg === 0 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
+            <img class="object-cover w-full h-full" :src="currentPair.image2" />
+          </div>
+          <div :class="{ 'selected-right-text': $store.state.favoriteSelectedImg === 1, 'hidden':  $store.state.favoriteSelectedImg === 0 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl">{{ currentPair.title2 }}</div>
+          <div v-if="this.$store.state.favoriteSelectedImg == 0" class="h-24 absolute top-72 inset-0 flex items-center justify-center">
+            <img v-show="this.$store.state.favoriteSelectedImg == 0" class="w-24 h-24 absolute sm:-translate-y-18" src="/image/vs.png" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -37,17 +39,34 @@
 
 <script>
 import FinishModal from './FavoriteFinishModal.vue'
+
 export default {
   components: {
     FinishModal
   },
-  data () {
-    return {
+  mounted() {
+    this.$store.commit('resetFavoriteRoundFinish')
+    this.$store.dispatch('getFavoriteData')
+    this.$store.dispatch('setFavoriteRankMax')
+  },
+  methods: {
+    selectedImg(index) {
+      this.$store.dispatch('selectFavoriteImgIndex', index);
     }
   },
-  mounted() {
-    this.$store.dispatch('getFavoriteData')
-  },
+  computed: {
+    currentPair() {
+      if (this.$store.state.favoriteImagePairs.length === 0 || this.$store.state.favoriteCurrentPairIndex >= this.$store.state.favoriteImagePairs.length) {
+        return {
+          image1: '',
+          image2: '',
+          title1: '',
+          title2: ''
+        };
+      }
+      return this.$store.state.favoriteImagePairs[this.$store.state.favoriteCurrentPairIndex];
+    }
+  }
 }
 </script>
 
@@ -66,25 +85,25 @@ export default {
     -3px 2px 0 white, 3px 2px 0 white;
 }
 .selected-left {
-  transition: transform 0.4s ease, left 0.4s ease;
+  transition: transform 0.5s ease, left 0.5s ease;
   transform: scale(1.1);
   transform: translateX(25%);
   z-index: 40;
 }
 .selected-left-text {
-  transition: transform 0.4s ease;
+  transition: transform 0.5s ease;
   transform: translate(25%, -350%);
   z-index: 50;
   font-weight: 700;
   font-size: 2rem;
 }
 .selected-right {
-  transition: transform 0.4s ease, right 0.4s ease;
+  transition: transform 0.5s ease, right 0.5s ease;
   transform: scale(1.1);
   transform: translateX(-25%);
 }
 .selected-right-text {
-  transition: transform 0.4s ease;
+  transition: transform 0.5s ease;
   transform: translate(-25%, -350%);
   z-index: 50;
   font-weight: 700;
@@ -94,12 +113,12 @@ export default {
   opacity: 0;
 }
 .unselected-left {
-  transition: transform 0.4s ease;
+  transition: transform 0.5s ease;
   transform: scale(0.5);
   z-index: 0;
 }
 .unselected-right {
-  transition: transform 0.4s ease;
+  transition: transform 0.5s ease;
   transform: scale(0.5);
 }
 </style>
