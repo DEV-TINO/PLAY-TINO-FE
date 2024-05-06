a614b6c889670fea7defa48188dcf4f0db923401
김민지 <alswlk0306@gmail.com>
2024-05-06 22:14:48 +0900

    fix: image click event, disabled image click

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -16,16 +16,16 @@
         </div>
         <div class="w-full text-white text-xl flex justify-center items-center pt-5">{{ this.$store.state.favoriteGameRound }}</div>
       </div>
-      <div class="w-full h-full mt-2 flex justify-center relative min-w-min">
+      <div :class="{ 'click-disabled': this.isDisabled() == true }" class="w-full h-full mt-2 flex justify-center relative min-w-min">
         <div @click="selectedImg(0)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0 }" class="flex flex-col justify-center items-end cursor-pointer">
           <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0, 'unselected-left': this.$store.state.favoriteSelectedImg === 1 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <img @error="handleImageError($event)" class="object-cover w-full h-full bg-gray-200" :src="currentPair.image1" />
+            <img @error="handleImageError($event)" class="object-cover w-full h-full" :src="currentPair.image1" />
           </div>
           <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === 0, 'hidden': this.$store.state.favoriteSelectedImg === 1 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title1 }}</div>
         </div>
         <div @click="selectedImg(1)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1 }" class="flex flex-col justify-center cursor-pointer">
           <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1, 'unselected-right': this.$store.state.favoriteSelectedImg === 0 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <img @error="handleImageError($event)" class="object-cover w-full h-full bg-gray-200" :src="currentPair.image2" />
+            <img @error="handleImageError($event)" class="object-cover w-full h-full" :src="currentPair.image2" />
           </div>
           <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === 1, 'hidden': this.$store.state.favoriteSelectedImg === 0 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title2 }}</div>
           <div v-if="this.$store.state.favoriteSelectedImg == 0" class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center pointer-events-none">
@@ -49,11 +49,6 @@ export default {
     this.$store.commit('resetFavoriteRoundFinish')
     this.$store.dispatch('getFavoriteData')
     this.$store.dispatch('setFavoriteRankMax')
-  },
-  data() {
-    return {
-      isError: false
-    }
   },
   methods: {
     handleImageError(event) {
