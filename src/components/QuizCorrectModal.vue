1ea3b4748184747efe0ca1256c986376a1ff3bcd
jeongyun <jeong.yun@tukorea.ac.kr>
2024-05-18 15:26:15 +0900

    fix : block draging

--- src/components/QuizCorrectModal.vue
+++ src/components/QuizCorrectModal.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="w-full h-full bg-modal-background fixed flex justify-center items-center">
     <div class="w-auto h-auto bg-white rounded-2xl py-4 md:py-6 lg:py-8 px-24 md:px-28 lg:px-32 min-w-96">
-      <div class="flex justify-center gap-1 items-baseline pt-1">
+      <div class="flex justify-center gap-1 items-baseline pt-1 select-none">
         <div class="text-xl md:text-2xl font-bold text-correct-modal">{{ quizIndex }}</div>
         <div class="text-sm md:text-base text-primary">/ 10</div>
       </div>
-      <div class="flex justify-center">
+      <div class="flex justify-center select-none">
       <img class="py-8 object-cover w-52 md:w-56 lg:w-64" src="/o.png"/>
     </div>
-      <div class="flex justify-center pb-0.5 md:pb-1">
+      <div class="flex justify-center pb-0.5 md:pb-1 select-none">
         <div class="font-bold text-xl md:text-2xl text-primary py-1 md:pb-2">정답입니다</div> 
       </div>
       <div class="flex justify-center py-0.5 md:py-1">
-        <div class="w-16 md:w-20 h-6 md:h-8 items-center rounded-lg bg-correct-modal flex justify-center font-bold text-white cursor-pointer" @click="confirm()">
+        <div class="w-16 md:w-20 h-6 md:h-8 items-center select-none rounded-lg bg-correct-modal flex justify-center font-bold text-white cursor-pointer" @click="confirm()">
           <div>확인</div>
         </div>
       </div>
--- src/components/QuizWrongModal.vue
+++ src/components/QuizWrongModal.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="w-full h-full bg-modal-background fixed flex justify-center items-center">
       <div class="w-auto h-auto bg-white rounded-2xl py-4 md:py-6 lg:py-8 px-24 md:px-28 lg:px-32 min-w-96">
-        <div class="flex justify-center gap-1 items-baseline pt-1">
+        <div class="flex justify-center gap-1 items-baseline pt-1 select-none">
           <div class="text-xl md:text-2xl font-bold text-wrong-modal">{{ quizIndex }}</div>
           <div class="text-sm md:text-base text-primary">/ 10</div>
         </div>
         <div class="flex justify-center">
-        <img class="py-8 object-cover w-52 md:w-56 lg:w-64" src="/x.png"/>
+        <img class="py-8 object-cover w-52 md:w-56 lg:w-64 select-none" src="/x.png"/>
       </div>
-        <div class="flex justify-center pb-0.5 md:pb-1">
+        <div class="flex justify-center pb-0.5 md:pb-1 select-none">
           <div class="font-bold text-xl md:text-2xl text-primary py-1 md:pb-2">오답입니다</div> 
         </div>
         <div class="flex justify-center pb-2">
-          <div class="flex justfy-center gap-2">
+          <div class="flex justfy-center gap-2 select-none">
             <div>정답 :</div>
             <div>{{ this.correctAnswer }}</div>
           </div>
         </div>
         <div class="flex justify-center py-0.5 md:py-1">
-          <div class="w-16 md:w-20 h-6 md:h-8 items-center rounded-lg bg-wrong-modal flex justify-center font-bold text-white cursor-pointer" @click="confirm()">
+          <div class="select-none w-16 md:w-20 h-6 md:h-8 items-center rounded-lg bg-wrong-modal flex justify-center font-bold text-white cursor-pointer" @click="confirm()">
             <div>확인</div>
           </div>
         </div>
