03a5f05c33ec714b13e345718a0e5b00cd41ad75
jeongyun <jeong.yun@tukorea.ac.kr>
2024-05-22 16:15:06 +0900

    refactor : remove useless lines

--- src/components/TimerPage.vue
+++ src/components/TimerPage.vue
@@ -75,9 +75,6 @@ import axios from 'axios'
       this.openModal = true
     },
     start() {
-      if (parseFloat(this.time) != 0) {
-        alert("멈춤 버튼을 눌러주세요")
-      }
       if (this.running) return
       if (this.timeBegan == null) {
         this.timeBegan = new Date()
@@ -86,10 +83,6 @@ import axios from 'axios'
       this.running = true
     },
     stop() {
-      if (parseFloat(this.time) == 0) {
-        alert("시작 버튼을 먼저 눌러주세요")
-        return
-      }
       this.running = false
       this.timeStopped = new Date()
       clearInterval(this.started)
