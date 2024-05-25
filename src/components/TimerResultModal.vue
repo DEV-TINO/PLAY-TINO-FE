409a9f7a3c2e180cd796c9a1124843739223c2a1
jeongyun <jeong.yun@tukorea.ac.kr>
2024-05-25 17:59:52 +0900

    feat : change result modals to responsive design

--- src/components/TimerResultModal.vue
+++ src/components/TimerResultModal.vue
@@ -1,27 +1,27 @@
 <template>
-  <div class="w-full h-full bg-modal-background bg-opacity-50 fixed flex justify-center items-center z-50">
+  <div class="w-full h-full bg-modal-background bg-opacity-50 fixed flex justify-center items-center">
     <div class="bg-white rounded-3xl">
-      <div class="flex flex-col items-center pt-10 pb-10 pr-24 pl-24 m-2 border-4 border-timer-stop rounded-3xl select-none">
-        <div class="text-2xl font-extrabold text-primary py-3">정각을 맞춰라</div>
-        <div class="text-4xl font-bold text-timer-stop pt-2">축하드려요!</div>
-        <div class="text-4xl font-bold text-timer-stop pb-2">랭킹에 오르셨어요</div>
+      <div class="flex flex-col items-center py-4 sm:py-8 md:py-10 px-12 sm:px-20 md:px-24 sm:m-2 sm:border-3 md:border-4 border-timer-stop rounded-3xl select-none">
+        <div class="text-lg sm:text-xl md:text-2xl font-extrabold text-primary py-1 sm:py-2 md:py-3">정각을 맞춰라</div>
+        <div class="text-2xl sm:text-3xl md:text-4xl font-bold text-timer-stop pt-1 sm:pt-2">축하드려요!</div>
+        <div class="text-2xl sm:text-3xl md:text-4xl font-bold text-timer-stop pb-1 sm:pb-2">랭킹에 오르셨어요</div>
         <div class="flex py-4">
-          <hr class="w-8 h-1 bg-timer-stop">
+          <hr class="w-8 h-0.5 sm:h-1 bg-timer-stop">
         </div>
         <div class="flex flex-col items-center mb-5">
-          <div class="flex gap-4 items-center py-2 select-none">
-            <div class="text-2xl font-semibold text-primary">기록</div>
-            <div class="text-timer-stop text-2xl font-semibold">{{ this.stopTime }}</div>
+          <div class="flex gap-4 items-center sm:py-1 md:py-2 select-none">
+            <div class="text-lg sm:text-xl md:text-2xl font-semibold text-primary">기록</div>
+            <div class="text-timer-stop text-lg sm:text-xl md:text-2xl font-semibold">{{ this.stopTime }}</div>
           </div>
-          <div class="flex gap-4 pb-3 items-center select-none">
-            <div class="text-2xl font-semibold text-primary">오차</div>
-            <div class="text-timer-stop text-2xl font-semibold">{{ calculateError() }}</div>
+          <div class="flex gap-4 pb-1 sm:pb-2 md:pb-3 items-center select-none">
+            <div class="text-lg sm:text-xl md:text-2xl font-semibold text-primary">오차</div>
+            <div class="text-timer-stop text-lg sm:text-xl md:text-2xl font-semibold">{{ calculateError() }}</div>
           </div>
         </div>
-        <div class="flex items-center gap-4 select-none">
-          <div @click="handleRouterRank()" class="bg-timer-stop text-white border-timer-stop border-2 pr-5 pl-5 pt-1 pb-1 rounded-lg flex justify-center font-bold text-xl cursor-pointer">확인</div>
-          <div @click="reload()" class="bg-white border-timer-stop border-2 pr-4 pl-4 pt-1 pb-1 rounded-lg text-timer-stop font-bold text-xl cursor-pointer">다시하기</div>
-          <div @click="handleRouterMain()" class="bg-white border-timer-stop border-2 pr-4 pl-4 pt-1 pb-1 rounded-lg text-timer-stop font-bold text-xl cursor-pointer">메인으로</div>
+        <div class="flex flex-col sm:flex-row items-center gap-2 md:gap-4 select-none w-full pb-2 sm:pb-0">
+          <div @click="handleRouterRank()" class="w-full sm:w-auto bg-timer-stop text-white border-timer-stop border-2 py-0.5 sm:px-5 sm:py-1 rounded-lg flex justify-center font-bold text-base sm:text-xl cursor-pointer">확인</div>
+          <div @click="reload()" class="w-full sm:w-auto bg-white border-timer-stop border-2 sm:px-4 py-0.5 sm:py-1 rounded-lg text-timer-stop font-bold text-base sm:text-xl flex justify-center cursor-pointer">다시하기</div>
+          <div @click="handleRouterMain()" class="w-full sm:w-auto bg-white border-timer-stop border-2 sm:px-4 py-0.5 sm:py-1 rounded-lg text-timer-stop font-bold text-base sm:text-xl flex justify-center cursor-pointer">메인으로</div>
         </div>
       </div>
     </div>
