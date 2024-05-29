cb554445780cf58c7674ee5031243e141ef722e4
김민지 <alswlk0306@gmail.com>
2024-05-29 13:52:17 +0900

    refactor: use Array for loaded variable

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -19,15 +19,15 @@
       <div :class="{ 'click-disabled': isDisabled() }" class="w-full h-full mt-2 flex justify-center relative min-w-min">
         <div @click="selectedImg(0)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0 }" class="flex flex-col justify-center items-end cursor-pointer">
           <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0, 'unselected-left': this.$store.state.favoriteSelectedImg === 1 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <div v-if="!imageLoaded1" class="skeleton-loader"></div>
-            <img v-show="imageLoaded1" @load="handleImageLoad(this.pair1)" @error="handleImageError(1)" class="object-cover w-full h-full" :src="currentPair.image1" />
+            <div v-if="!imagePair[0]" class="skeleton-loader"></div>
+            <img v-show="imagePair[0]" @load="handleImageLoad(this.pair1)" @error="handleImageError(1)" class="object-cover w-full h-full" :src="currentPair.image1" />
           </div>
           <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === 0, 'hidden': this.$store.state.favoriteSelectedImg === 1 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title1 }}</div>
         </div>
         <div @click="selectedImg(1)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1 }" class="flex flex-col justify-center cursor-pointer">
           <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1, 'unselected-right': this.$store.state.favoriteSelectedImg === 0 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <div v-if="!imageLoaded2" class="skeleton-loader"></div>
-            <img v-show="imageLoaded2" @load="handleImageLoad(this.pair2)" @error="handleImageError(2)" class="object-cover w-full h-full" :src="currentPair.image2" />
+            <div v-if="!imagePair[1]" class="skeleton-loader"></div>
+            <img v-show="imagePair[1]" @load="handleImageLoad(this.pair2)" @error="handleImageError(2)" class="object-cover w-full h-full" :src="currentPair.image2" />
           </div>
           <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === 1, 'hidden': this.$store.state.favoriteSelectedImg === 0 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title2 }}</div>
           <div v-if="this.$store.state.favoriteSelectedImg == 0" class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center pointer-events-none">
@@ -48,6 +48,7 @@ export default {
   },
   data() {
     return {
+      imagePair: [false, false],
       imageLoaded1: false,
       imageLoaded2: false,
       pair1: 1,
@@ -70,16 +71,16 @@ export default {
     },
     handleImageError(index) {
       if (index === 1) {
-        this.imageLoaded1 = false
+        this.imagePair[0] = false
       } else if (index === 2) {
-        this.imageLoaded2 = false
+        this.imagePair[1] = false
       }
     },
     handleImageLoad(index) {
       if (index === 1) {
-        this.imageLoaded1 = true
+        this.imagePair[0] = true
       } else if (index === 2) {
-        this.imageLoaded2 = true
+        this.imagePair[1] = true
       }
     },
     selectedImg(index) {
