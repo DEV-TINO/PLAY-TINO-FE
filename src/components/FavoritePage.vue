fe35bbaa66354bbdf2f7e32b729f7d245172a66f
김민지 <alswlk0306@gmail.com>
2024-05-29 14:46:13 +0900

    refactor: change magic number to object type

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -17,21 +17,21 @@
         <div class="w-full text-white text-xl flex justify-center items-center pt-5">{{ this.$store.state.favoriteGameRound }}</div>
       </div>
       <div :class="{ 'click-disabled': isDisabled() }" class="w-full h-full mt-2 flex justify-center relative min-w-min">
-        <div @click="selectedImg(0)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0 }" class="flex flex-col justify-center items-end cursor-pointer">
-          <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === 0, 'unselected-left': this.$store.state.favoriteSelectedImg === 1 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <div v-if="!imagePair[0]" class="skeleton-loader"></div>
-            <img v-show="imagePair[0]" @load="handleImageLoad(this.pair1)" @error="handleImageError(this.pair1)" class="object-cover w-full h-full" :src="currentPair.image1" />
+        <div @click="selectImage(this.imageIndex.left)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="flex flex-col justify-center items-end cursor-pointer">
+          <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'unselected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
+            <div v-if="!imagePair[this.imageIndex.left]" class="skeleton-loader"></div>
+            <img v-show="imagePair[this.imageIndex.left]" @load="handleImageLoad(this.imageIndex.left)" @error="handleImageError(this.imageIndex.left)" class="object-cover w-full h-full" :src="currentPair.image1" />
           </div>
-          <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === 0, 'hidden': this.$store.state.favoriteSelectedImg === 1 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title1 }}</div>
+          <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title1 }}</div>
         </div>
-        <div @click="selectedImg(1)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1 }" class="flex flex-col justify-center cursor-pointer">
-          <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === 1, 'unselected-right': this.$store.state.favoriteSelectedImg === 0 }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
-            <div v-if="!imagePair[1]" class="skeleton-loader"></div>
-            <img v-show="imagePair[1]" @load="handleImageLoad(this.pair2)" @error="handleImageError(this.pair2)" class="object-cover w-full h-full" :src="currentPair.image2" />
+        <div @click="selectImage(this.imageIndex.right)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="flex flex-col justify-center cursor-pointer">
+          <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'unselected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="w-favorite-content-width h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 min-h-72 overflow-hidden">
+            <div v-if="!imagePair[this.imageIndex.right]" class="skeleton-loader"></div>
+            <img v-show="imagePair[this.imageIndex.right]" @load="handleImageLoad(this.imageIndex.right)" @error="handleImageError(this.imageIndex.right)" class="object-cover w-full h-full" :src="currentPair.image2" />
           </div>
-          <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === 1, 'hidden': this.$store.state.favoriteSelectedImg === 0 }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title2 }}</div>
-          <div v-if="this.$store.state.favoriteSelectedImg == 0" class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center pointer-events-none">
-            <img v-show="this.$store.state.favoriteSelectedImg == 0" class="w-24 h-24 sm:-translate-y-18" src="/image/vs.png" />
+          <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="flex items-center justify-center w-favorite-content-width text-white text-2xl mt-3 min-w-72 sm:text-2xl selected-text-outline">{{ currentPair.title2 }}</div>
+          <div v-if="this.$store.state.favoriteSelectedImg === this.imageIndex.left" class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center pointer-events-none">
+            <img v-show="this.$store.state.favoriteSelectedImg === this.imageIndex.left" class="w-24 h-24 sm:-translate-y-18" src="/image/vs.png" />
           </div>
         </div>
       </div>
@@ -42,6 +42,11 @@
 <script>
 import FinishModal from './FavoriteFinishModal.vue'
 
+const ImageIndex = {
+  left: 0,
+  right: 1
+}
+
 export default {
   components: {
     FinishModal
@@ -49,12 +54,11 @@ export default {
   data() {
     return {
       imagePair: [false, false],
-      pair1: 1,
-      pair2: 2
+      imageIndex: ImageIndex
     }
   },
   mounted() {
-    if(this.$store.state.userId == '') {
+    if (this.$store.state.userId === '') {
       alert("로그인 없이 이용할 수 없습니다")
       this.$router.push(`/`)
       return
@@ -68,24 +72,15 @@ export default {
       this.$store.dispatch('setFavoriteRankMax')
     },
     handleImageError(index) {
-      if (index === 1) {
-        this.imagePair[0] = false
-      } else if (index === 2) {
-        this.imagePair[1] = false
-      }
+      this.imagePair[index] = false
     },
     handleImageLoad(index) {
-      if (index === 1) {
-        this.imagePair[0] = true
-      } else if (index === 2) {
-        this.imagePair[1] = true
-      }
+      this.imagePair[index] = true
     },
-    selectedImg(index) {
+    selectImage(index) {
       if (this.$store.state.favoriteSelectedImg !== '') {
         return
       }
-
       this.$store.dispatch('selectFavoriteImgIndex', index)
     },
     handleRouterMain() {
@@ -109,9 +104,8 @@ export default {
     }
   },
   watch: {
-    currentPair(newPair, oldPair) {
-      this.imageLoaded1 = false
-      this.imageLoaded2 = false
+    currentPair() {
+      this.imagePair = [false, false]
     }
   }
 }
