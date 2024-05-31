7497fb8b0e3b6295483c386da78cec039827d93b
김민지 <alswlk0306@gmail.com>
2024-06-01 03:58:17 +0900

    style: modify favorite game mobile design, image title

--- src/components/FavoritePage.vue
+++ src/components/FavoritePage.vue
@@ -16,20 +16,20 @@
         </div>
         <div class="w-full text-white text-sm pb-4 md:text-xl flex justify-center items-center md:pt-5">{{ this.$store.state.favoriteGameRound }}</div>
       </div>
-      <div :class="{ 'click-disabled': isDisabled() }" class="w-full h-auto xl:h-full mt-2 flex justify-center relative min-w-50">
+      <div :class="{ 'click-disabled': isDisabled() }" class="w-full h-auto xl:h-full mt-2 mb-5 flex justify-center relative min-w-50">
         <div @click="selectImage(this.imageIndex.left)" :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="flex flex-col lg:justify-center items-center cursor-pointer">
-          <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'unselected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="w-44 h-44 xl:w-favorite-content-width xl:h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 sm:min-h-72 overflow-hidden">
+          <div :class="{ 'selected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'unselected-left': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="w-40 h-40 xl:w-favorite-content-width xl:h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 sm:min-h-72 overflow-hidden">
             <div v-if="!imagePair[this.imageIndex.left]" class="skeleton-loader"></div>
             <img v-show="imagePair[this.imageIndex.left]" @load="handleImageLoad(this.imageIndex.left)" @error="handleImageError(this.imageIndex.left)" class="object-cover w-full h-full" :src="currentPair.image1" />
           </div>
-          <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="flex items-center justify-center xl:w-favorite-content-width text-white min-w-40 text-xs sm:text-sm mt-1 md:mt-3 md:text-lg lg:text-xl xl:text-2xl md:min-w-72 selected-text-outline">{{ currentPair.title1 }}</div>
+          <div :class="{ 'selected-left-text': this.$store.state.favoriteSelectedImg === this.imageIndex.left, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="flex text-xs items-center justify-center xl:w-favorite-content-width text-white min-w-40 text-xs sm:text-sm mt-1 md:mt-3 md:text-lg lg:text-xl xl:text-2xl md:min-w-72 selected-text-outline">{{ currentPair.title1 }}</div>
         </div>
         <div @click="selectImage(this.imageIndex.right)" :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.right }" class="flex flex-col lg:justify-center cursor-pointer">
-          <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'unselected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="w-44 h-44 xl:w-favorite-content-width xl:h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 sm:min-h-72 overflow-hidden">
+          <div :class="{ 'selected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'unselected-right': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="w-40 h-40 xl:w-favorite-content-width xl:h-favorite-content-height aspect-w-1 aspect-h-1 border-8 cursor-pointer sm:min-w-72 sm:min-h-72 overflow-hidden">
             <div v-if="!imagePair[this.imageIndex.right]" class="skeleton-loader"></div>
             <img v-show="imagePair[this.imageIndex.right]" @load="handleImageLoad(this.imageIndex.right)" @error="handleImageError(this.imageIndex.right)" class="object-cover w-full h-full" :src="currentPair.image2" />
           </div>
-          <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="flex items-center justify-center xl:w-favorite-content-width text-white min-w-40 text-xs sm:text-sm mt-1 md:mt-3 md:text-lg md:text-md lg:text-xl xl:text-2xl md:min-w-72 selected-text-outline">{{ currentPair.title2 }}</div>
+          <div :class="{ 'selected-right-text': this.$store.state.favoriteSelectedImg === this.imageIndex.right, 'hidden': this.$store.state.favoriteSelectedImg === this.imageIndex.left }" class="flex text-xs items-center justify-center xl:w-favorite-content-width text-white min-w-40 text-xs sm:text-sm mt-1 md:mt-3 md:text-lg md:text-md lg:text-xl xl:text-2xl md:min-w-72 selected-text-outline">{{ currentPair.title2 }}</div>
           <div v-if="this.$store.state.favoriteSelectedImg === ''" class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center pointer-events-none">
             <img class="w-8 h-8 sm:w-16 sm:h-16 md:w-16 md:h-16 lg:w-20 lg:h-20 sm:-translate-y-18" src="/image/vs.png" />
           </div>
@@ -146,20 +146,22 @@ export default {
   z-index: 40;
 }
 .selected-left-text {
+  font-size: 0.825rem;
   transition: transform 0.5s ease;
   transform: translate(25%, -350%);
   z-index: 50;
-  font-size: 2rem;
+  font-weight: 700;
 }
 .selected-right {
   transition: transform 0.5s ease, right 0.5s ease;
   transform: translateX(-25%);
 }
 .selected-right-text {
+  font-size: 0.825rem;
   transition: transform 0.5s ease;
   transform: translate(-25%, -350%);
   z-index: 50;
-  font-size: 2rem;
+  font-weight: 700;
 }
 .hidden {
   opacity: 0;
@@ -193,4 +195,41 @@ export default {
     background-color: #e0e0e0;
   }
 }
+@media (min-width: 640px) {
+  .selected-left-text {
+    font-size: 1rem;
+    transition: transform 0.5s ease;
+    transform: translate(45%, -350%);
+  }
+  .selected-right-text {
+    font-size: 1rem;
+    transition: transform 0.5s ease;
+    transform: translate(-25%, -350%);
+  }
+} 
+@media (min-width: 768px) {
+  .selected-left-text {
+    font-size: 1.25rem;
+    transition: transform 0.5s ease;
+    transform: translate(25%, -350%);
+  }
+  .selected-right-text {
+    font-size: 1.25rem;
+    transition: transform 0.5s ease;
+    transform: translate(-25%, -350%);
+  }
+}
+@media (min-width: 1024px) {
+  .selected-left-text {
+    font-size: 1.75rem;
+    transition: transform 0.5s ease;
+    transform: translate(25%, -350%);
+  }
+  .selected-right-text {
+    font-size: 1.75rem;
+    transition: transform 0.5s ease;
+    transform: translate(-25%, -350%);
+  }
+}
 </style>
+// 내가 보기엔 ! 최소값을 기본 애니메이션으로 지정한 후에, 나머지를 media 사용해서 하는 게 맞는 거 같군!
