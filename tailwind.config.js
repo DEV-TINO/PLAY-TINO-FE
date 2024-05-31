cd3a48fc0d961c9b276f47aa4f271ab5e5a9419d
김민지 <alswlk0306@gmail.com>
2024-06-01 04:10:58 +0900

    style: add font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,16 @@ module.exports = {
       '16': '16px',
       '24': '24px'
     },
+    fontSize: {
+      'sm': '0.8rem',
+      'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      'favortire-default-title': '0.625rem'
+    }
   },
   plugins: [
   ],
