5233adfcfb83266775a0dca904bff2784e5693d5
jeongyun <jeong.yun@tukorea.ac.kr>
2024-06-01 13:59:30 +0900

    fix : fix pagination error

--- src/components/TimerStatisticPage.vue
+++ src/components/TimerStatisticPage.vue
@@ -101,21 +101,18 @@ export default {
         this.showPagination = false
       } else if (this.pageCount < ELLIPSIS_NEED) {
         this.pages = this.createArray(this.pageCount)
+        this.showPagination = true
         this.showStartEllipsis = false
         this.showEndEllipsis = false
         return
-      }
-      else if(this.currentPage == FIRST_PAGE) {
+      } else if(this.currentPage == FIRST_PAGE) {
         this.pages = [FIRST_PAGE, FIRST_PAGE + 1, FIRST_PAGE + 2]
-        this.showPagination = true
-      }
-      else if(this.currentPage == this.pageCount) {
+        this.showStartEllipsis = true
+        this.showEndEllipsis = true
+      } else if(this.currentPage == this.pageCount) {
         this.pages = [this.pageCount - 2, this.pageCount - 1, this.pageCount]
-        this.showPagination = true
-      }
-      else if(this.currentPage > 3 || this.pageCount - 2) {
+      } else if(this.currentPage > 3 || this.pageCount - 2) {
         this.pages = [this.currentPage - 1, this.currentPage, this.currentPage + 1]
-        this.showPagination = true
       }
       this.showStartEllipsis = this.currentPage > 2
       this.showEndEllipsis = this.currentPage < (this.pageCount - 1)
